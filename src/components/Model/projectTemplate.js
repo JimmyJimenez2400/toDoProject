93a305864af262de8029e6039ba30b265eef4d34
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-05-20 04:06:06 -0400

    Add class called Project to create instanes of an object from constructor call

--- src/components/Model/projectTemplate.js
+++ src/components/Model/projectTemplate.js
@@ -1,29 +1,49 @@
+/* eslint-disable no-dupe-class-members */
 // For Folder Names
-export default class FolderProjectsTitle {
+class Project {
+  projectArray = [];
+
+  title;
+
   constructor(title) {
     this.title = title;
   }
 
   get title() {
-    return this._title;
+    return this.title;
   }
 
   set title(value) {
-    this._title = value;
+    this.title = value;
   }
 
-  removeProjectFromLibrary() {
-    const getSpecificIndex = projectLibrary.indexOf(this);
-    // const getTitle = projectLibrary[getSpecificIndex].title;
-    console.log(getSpecificIndex);
-    projectLibrary.splice(getSpecificIndex, 1);
-    console.table(projectLibrary);
-    // console.log(getTitle);
-    // console.log(`removing ${this.title} from library`);
-    // const filterArray = projectLibrary.filter((x) => x.title !== getTitle);
-    // console.log(`${filterArray} HELLO`);
-    // console.log(projectLibrary);
+  pushToProjects(item) {
+    this.projectArray.append(item);
+  }
 
-    // We need to remove that specific project from the library
+  removeFromProjects(item) {
+    // We want to remove specific item from array
+    // Give Specific ID to each Task
+  }
+
+  get Projects() {
+    return this.projectArray;
   }
 }
+
+// removeProjectFromLibrary() {
+//   const getSpecificIndex = projectLibrary.indexOf(this);
+//   // const getTitle = projectLibrary[getSpecificIndex].title;
+//   console.log(getSpecificIndex);
+//   projectLibrary.splice(getSpecificIndex, 1);
+//   console.table(projectLibrary);
+//   // console.log(getTitle);
+//   // console.log(`removing ${this.title} from library`);
+//   // const filterArray = projectLibrary.filter((x) => x.title !== getTitle);
+//   // console.log(`${filterArray} HELLO`);
+//   // console.log(projectLibrary);
+
+//   // We need to remove that specific project from the library
+// }
+
+export default Project;
