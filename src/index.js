13d0a28303c275d5c7c7e30eff69dc3b8cb4c660
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-05-20 04:07:20 -0400

    Testing classes in console for now

--- src/index.js
+++ src/index.js
@@ -1,11 +1,32 @@
 import pageLoad from "./pageLoad/pageLoad";
-import folderToDoList from "./components/Model/folderTodoLibrary";
 import "./style.css";
+
+import Project from "./components/Model/projectTemplate";
+import Task from "./components/Model/taskTemplate";
+import Collection from "./components/Model/collectionTemplate";
 
 pageLoad();
 
-console.log(folderToDoList.folderLibrary);
-console.log(folderToDoList.pushFolders_To_ToDoLibrary());
-console.log(folderToDoList.folderLibrary);
+const HomeFolder = new Project("Home");
+console.log(HomeFolder);
 
-folderToDoList.iteratingOverArray();
+const cleanToilet = new Task("Clean Toilet", "5/20/2023", "High");
+console.log(cleanToilet);
+HomeFolder.projectArray.push(cleanToilet);
+console.log(HomeFolder);
+
+const cleanRoom = new Task("Clean Room", "5/20/2023", "Medium");
+
+const cleanDesk = new Task("Clean Desk", "5/20/2023", "Low");
+
+HomeFolder.projectArray.push(cleanDesk, cleanRoom);
+console.log(HomeFolder.projects);
+
+const SchoolFolder = new Project("School");
+console.log(SchoolFolder);
+
+const FolderContainer = new Collection();
+FolderContainer.items.push(HomeFolder);
+FolderContainer.items.push(SchoolFolder);
+
+console.log(FolderContainer.items);
