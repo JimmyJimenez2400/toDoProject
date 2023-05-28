238c39082c470a7f778c54f3a3c4423a724405a9
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-05-27 23:10:45 -0400

    Add removeProjectFromProjects method to remove element from projects array

--- src/components/Model/collection.js
+++ src/components/Model/collection.js
@@ -1,10 +1,24 @@
-const projects = [];
+let projects = [];
 
 const addProjectToProjects = (item) => {
   // push to projects array
   projects.push(item);
 };
 
+const removeProjectFromProjects = (item) => {
+  console.log(`Calling Projects before filter: ${projects}`);
+  projects = projects.filter((object) => object.id !== item.id);
+  console.log(`Calling Projects after filter: ${projects}`);
+};
+
+const setProjects = (value) => projects.push(value);
+
 const getProjects = () => projects;
 
-export { addProjectToProjects, getProjects };
+export {
+  addProjectToProjects,
+  getProjects,
+  projects,
+  setProjects,
+  removeProjectFromProjects,
+};
