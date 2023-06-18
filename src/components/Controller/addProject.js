c69605626c686e6bd6e77d9b95911669491e40f1
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-06-18 02:45:44 -0400

    Add function that will allow user to submit a project with form

--- src/components/Controller/addProject.js
+++ src/components/Controller/addProject.js
@@ -1,20 +1,10 @@
 import { createProject } from "../Model/collection";
-import createProjectForm from "../View/createProjectForm";
 
-export default function addProject(title, description) {
-  // we need to grab the button for projec
-  createProject(title, description);
-  createProjectForm();
+export default function addProject(e) {
+  e.preventDefault();
 
-  const formOpen = document.querySelector(".dialogContainer");
-  formOpen.showModal();
+  const titleInput = document.getElementById("title");
+  const descriptionInput = document.getElementById("description");
 
-  const closeForm = document.querySelector(".cancelButton");
-  closeForm.addEventListener("click", () => {
-    formOpen.remove();
-  });
-
-  // add an event listener which will call our class
-  // then call our view `project` html\
-  console.log("HELLO");
+  return createProject(titleInput.value, descriptionInput.value);
 }
