8c1cc4b911d5a27076ada6192f96703e7b8db6e4
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-06-18 02:47:31 -0400

    Adding event listeners to elements

--- src/pageLoad/pageLoad.js
+++ src/pageLoad/pageLoad.js
@@ -1,13 +1,26 @@
 import addProject from "../components/Controller/addProject";
+import openForm from "../components/Controller/openForm";
 
 export default function pageLoad() {
   const content = document.querySelector("#content");
 
   const addProjectButton = document.querySelector(".addProject");
+  const projectForm = document.getElementById("projectForm");
 
-  addProjectButton.addEventListener("click", () => {
-    addProject("NEW TITLE", "YOOOOOOO WHAT IS HAPPENING");
-  });
+
+
+  addProjectButton.addEventListener("click", openForm);
+
+  projectForm.addEventListener("submit", addProject)
+
+
+
+
+  // addProjectButton.addEventListener("click", () => {
+  //   const getTitleInput = document.getElementById("title").value;
+  //   console.log(getTitleInput);
+
+  // });
 
   return content;
 }
