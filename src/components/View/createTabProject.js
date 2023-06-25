4334793c012f9bf1b5923263dc9fd0571a4b0e8e
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-06-25 16:58:45 -0400

    Add event listeners to li elements to simulate tab switching

--- src/components/View/createTabProject.js
+++ src/components/View/createTabProject.js
@@ -3,6 +3,7 @@ export default function createProjectTab(titleInput, itemID) {
   const projectUL = document.querySelector(".projects");
 
   const liElementContainer = document.createElement("li");
+  liElementContainer.setAttribute("class", "liElement");
   liElementContainer.setAttribute("id", `${itemID}`);
 
   const createTitle = document.createElement("h6");
@@ -11,6 +12,26 @@ export default function createProjectTab(titleInput, itemID) {
   liElementContainer.appendChild(createTitle);
 
   projectUL.appendChild(liElementContainer);
+
+  liElementContainer.addEventListener("click", (e) => {
+    const test = document.querySelector(".currentPage").children;
+
+    console.log(e.currentTarget.id);
+    const currentID = e.currentTarget.id;
+
+    for (let i = 0; i < test.length; i += 1) {
+      // console.log(test[i].getAttribute("project-id"));
+      if (currentID === test[i].getAttribute("project-id")) {
+        test[i].classList.add("ONLINE");
+        test[i].classList.remove("OFFLINE");
+      } else {
+        test[i].classList.add("OFFLINE");
+        test[i].classList.remove("ONLINE");
+      }
+    }
+
+    // Now, I need to make this specific ID from the mainPage be active or class active
+  });
 
   return projectUL;
 }
