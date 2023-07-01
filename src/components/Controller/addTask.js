6977bbf123a3ec8f8c99882e9ec2cb03f07269ad
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-06-30 22:26:00 -0400

    Add function that will add task Item to screen. still testing

---
+++ src/components/Controller/addTask.js
@@ -0,0 +1,28 @@
+import Project from "../Model/projectTemplate";
+
+export default function addItem(e) {
+  e.preventDefault();
+
+  console.log("PRESSING ADD ITEM BUTTON");
+
+  // const itemTask = document.getElementById("itemTask");
+  // const itemDate = document.getElementById("itemDate");
+  // const itemPriority = document.getElementById("itemPriority");
+  // const itemNote = document.getElementById("itemNote");
+
+  // const callItemMaker = (
+  //   itemTask.value,
+  //   itemDate.value,
+  //   itemPriority.value,
+  //   itemNote.value
+  // );
+
+  // // call call visual
+
+  // itemTask.value = "";
+  // itemDate.value = "";
+  // itemPriority.value = "";
+  // itemNote.value = "";
+
+  // call card visual from view
+}
