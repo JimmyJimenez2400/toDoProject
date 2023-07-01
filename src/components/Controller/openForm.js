6608a6a25f596072a6995d5643d0a14a299ce6f1
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-06-30 22:28:21 -0400

    Remove file from folder

--- src/components/Controller/openForm.js
+++
@@ -1,8 +0,0 @@
-import createProjectForm from "../View/createProjectForm";
-
-createProjectForm();
-
-export default function openForm() {
-  const formOpen = document.querySelector(".dialogContainer");
-  formOpen.showModal();
-}
