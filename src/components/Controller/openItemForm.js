4b5554d407e8bcd422ca2e6c9a172441293dccf0
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-02 03:32:17 -0400

    Create dialog ItemForm and function that will open dialog

--- src/components/Controller/openItemForm.js
+++ src/components/Controller/openItemForm.js
@@ -1,3 +1,8 @@
-import createItemForm from "../View/createItemForm";
+import createItemForm from "../View/createItem";
 
 createItemForm();
+
+export default function openItemForm() {
+  const formOpen = document.querySelector(".itemForm");
+  formOpen.showModal();
+}
