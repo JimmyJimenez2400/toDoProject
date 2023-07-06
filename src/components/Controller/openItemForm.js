5c1231305d33b692d6e047b95b47e607750a134d
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-06 02:30:11 -0400

    Modified file, put createItemForm function inside openItemForm instead of outside

--- src/components/Controller/openItemForm.js
+++ src/components/Controller/openItemForm.js
@@ -1,8 +1,7 @@
-import createItemForm from "../View/createItem";
-
-createItemForm();
+import createItemForm from "../View/createItemForm";
 
 export default function openItemForm() {
+  createItemForm();
   const formOpen = document.querySelector(".itemForm");
   formOpen.showModal();
 }
