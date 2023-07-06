63d4e7325b0815bdf1ea2156595d48d7a902f51f
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-06 02:34:44 -0400

    Add comments to clarify on what to do next for local storage

--- src/helpful_functions/localStorage.js
+++ src/helpful_functions/localStorage.js
@@ -6,7 +6,6 @@ function saveProjectSTodoLS(keyName, array) {
   localStorage.setItem(keyName, stringify);
 
   // now we need one for todo items
-  
 
   // we need to setItem for todo items, how do we grab that?
 }
@@ -19,6 +18,8 @@ function lookForDataInLS(keyName) {
   const getItem = localStorage.getItem(keyName);
 
   const data = JSON.parse(getItem);
+
+  // remember parse returns an object not an array
 
   return data;
 }
