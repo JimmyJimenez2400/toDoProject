6c87ed5428b902e048dc3522347ba0d548d1d68e
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-06 02:35:00 -0400

    remove comments

--- src/index.js
+++ src/index.js
@@ -2,7 +2,3 @@ import pageLoad from "./pageLoad/pageLoad";
 import "./style.css";
 
 pageLoad();
-// implement tab switching
-
-// We need to be able to click on the projects tabs by targeting ID perhaps?
-// Once clicked, we want that tab to have a class of "onlineTab" and the rest "offlineTab"
