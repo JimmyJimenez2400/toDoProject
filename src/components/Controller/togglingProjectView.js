f5f6ef77d90854ee06b8e5fc9f33febf48cc5960
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-11 18:03:01 -0400

    Made code into comments for now

---
+++ src/components/Controller/togglingProjectView.js
@@ -0,0 +1,19 @@
+// liElementContainer.addEventListener("click", (e) => {
+//   const test = document.querySelector(".currentPage").children;
+
+//   console.log(e.currentTarget.id);
+//   const currentID = e.currentTarget.id;
+
+//   for (let i = 0; i < test.length; i += 1) {
+//     // console.log(test[i].getAttribute("project-id"));
+//     if (currentID === test[i].getAttribute("data-projectid")) {
+//       test[i].classList.add("ONLINE");
+//       test[i].classList.remove("OFFLINE");
+//     } else {
+//       test[i].classList.add("OFFLINE");
+//       test[i].classList.remove("ONLINE");
+//     }
+//   }
+
+//   // Now, I need to make this specific ID from the mainPage be active or class active
+// });
