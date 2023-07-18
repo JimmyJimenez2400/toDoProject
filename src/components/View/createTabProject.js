0e0b488f73f9b5197418c18fe52c3c5bedb86804
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-18 01:44:00 -0400

    Remove edit and delete button from tab

--- src/components/View/createTabProject.js
+++ src/components/View/createTabProject.js
@@ -6,28 +6,10 @@ export default function createProjectTab(titleInput, itemID) {
   liElementContainer.setAttribute("class", "liElement");
   liElementContainer.setAttribute("id", `${itemID}`);
 
-  const leftSideContainer = document.createElement("section");
-  leftSideContainer.setAttribute("class", "leftSideElementTab");
-
-  const rightSideContainer = document.createElement("section");
-  rightSideContainer.setAttribute("class", "rightSideElementTab");
-
   const createTitle = document.createElement("h6");
   createTitle.textContent = `${titleInput}`;
 
-  const editButton = document.createElement("button");
-  editButton.textContent = "Edit";
-
-  const deleteButton = document.createElement("button");
-  deleteButton.textContent = "Delete";
-
-  leftSideContainer.appendChild(createTitle);
-
-  rightSideContainer.appendChild(editButton);
-  rightSideContainer.appendChild(deleteButton);
-
-  liElementContainer.appendChild(leftSideContainer);
-  liElementContainer.appendChild(rightSideContainer);
+  liElementContainer.appendChild(createTitle);
 
   projectUL.appendChild(liElementContainer);
 
