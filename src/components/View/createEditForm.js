5a7485aa916774063fdcd399cb88e2a828b3d4cb
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-18 01:43:27 -0400

    Create an edit form when user pressed on edit button

---
+++ src/components/View/createEditForm.js
@@ -0,0 +1,108 @@
+export default function createEditForm() {
+  const content = document.querySelector("#content");
+
+  const dialogContainer = document.createElement("dialog");
+  dialogContainer.setAttribute("class", "editItemForm");
+
+  const formContainer = document.createElement("form");
+  formContainer.classList.add("formContainerItem");
+
+  const topSection = document.createElement("section");
+  topSection.setAttribute("class", "topSectionItem formListItem");
+
+  const taskLabel = document.createElement("label");
+  taskLabel.setAttribute("for", "editTask");
+  taskLabel.textContent = "Task:";
+
+  const taskInput = document.createElement("input");
+  taskInput.setAttribute("id", "editTask");
+  taskInput.setAttribute("type", "text");
+
+  topSection.appendChild(taskLabel);
+  topSection.appendChild(taskInput);
+
+  topSection.appendChild(taskLabel);
+  topSection.appendChild(taskInput);
+
+  const middleSection = document.createElement("section");
+  middleSection.setAttribute("class", "middleSectionItem formListItem");
+
+  const textAreaLabel = document.createElement("label");
+  textAreaLabel.setAttribute("for", "editNote");
+  textAreaLabel.textContent = "Note:";
+
+  const textAreaInput = document.createElement("textarea");
+  textAreaInput.setAttribute("type", "text");
+  textAreaInput.setAttribute("id", "editNote");
+
+  middleSection.appendChild(textAreaLabel);
+  middleSection.appendChild(textAreaInput);
+
+  const secondMiddleSection = document.createElement("section");
+  secondMiddleSection.setAttribute("class", "secondMiddleSection formListItem");
+
+  const calendarInput = document.createElement("input");
+  calendarInput.setAttribute("id", "editDate");
+  calendarInput.type = "date";
+
+  const selectDropDownInput = document.createElement("select");
+  selectDropDownInput.setAttribute("id", "editSelect");
+  const lowTier = document.createElement("option");
+  lowTier.setAttribute = ("value", "low");
+  lowTier.textContent = "Low";
+
+  const mediumTier = document.createElement("option");
+  mediumTier.setAttribute = ("value", "medium");
+  mediumTier.textContent = "Medium";
+
+  const highTier = document.createElement("option");
+  highTier.setAttribute = ("value", "high");
+  highTier.textContent = "High";
+
+  selectDropDownInput.appendChild(lowTier);
+  selectDropDownInput.appendChild(mediumTier);
+  selectDropDownInput.appendChild(highTier);
+
+  secondMiddleSection.appendChild(calendarInput);
+  secondMiddleSection.appendChild(selectDropDownInput);
+
+  const bottomSection = document.createElement("section");
+  bottomSection.setAttribute("class", "bottomSectionItem formListItem");
+
+  const cancelButton = document.createElement("button");
+  cancelButton.setAttribute("formmethod", "dialog");
+
+  cancelButton.setAttribute("class", "editItemCancel");
+  cancelButton.textContent = "Cancel";
+
+  const submitButton = document.createElement("button");
+  submitButton.setAttribute("type", "submit");
+  submitButton.setAttribute("class", "editItemSubmit");
+  submitButton.textContent = "Submit";
+
+  bottomSection.appendChild(cancelButton);
+  bottomSection.appendChild(submitButton);
+
+  formContainer.appendChild(topSection);
+  formContainer.appendChild(middleSection);
+  formContainer.appendChild(secondMiddleSection);
+  formContainer.appendChild(bottomSection);
+
+  dialogContainer.appendChild(formContainer);
+
+  content.appendChild(dialogContainer);
+
+  return content;
+}
+
+/* 
+
+Input
+
+Input Note
+
+Date Input | Priority Input
+
+Cancel | Submit <- Will update card visuals
+
+*/
