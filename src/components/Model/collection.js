97a3ecb900fcda73b1c0cbeaf96dbd107ac3c1e1
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-07-24 08:19:47 -0400

    add imports, made sure to add items to correct places on DOM

--- src/components/Model/collection.js
+++ src/components/Model/collection.js
@@ -1,5 +1,8 @@
 import { lookForDataInLS } from "../../helpful_functions/localStorage";
 import Project from "./projectTemplate";
+import createMainTab from "../View/createMainTab";
+import createProjectTab from "../View/createTabProject";
+import createItemCard from "../View/createCardVisual";
 
 // eslint-disable-next-line import/no-mutable-exports
 let projects = [];
@@ -31,26 +34,63 @@ const readInformation = () => {
 // Still thinking about how I'll use setProjects
 const setProjects = (value) => projects.push(value);
 
-const getProjects = () => {
-  console.log(projects);
-};
+const getProjects = () => projects;
 
 const retrieveFromLocalStorage = (keyName) => {
   const arrayWithObjects = lookForDataInLS(keyName);
 
-  console.log(arrayWithObjects);
-
   for (let i = 0; i < arrayWithObjects.length; i += 1) {
-    console.log(arrayWithObjects[i]);
     const item = new Project(
       arrayWithObjects[i].title,
       arrayWithObjects[i].description,
       arrayWithObjects[i].id
     );
 
-    setProjects(item);
+    item.tasks = arrayWithObjects[i].task;
 
-    getProjects();
+    setProjects(item);
+  }
+
+  const test = getProjects();
+
+  for (let start = 0; start < test.length; start += 1) {
+    createProjectTab(test[start].title, test[start].id);
+    createMainTab(test[start].title, test[start].description, test[start].id);
+    for (let j = 0; j < test[start].tasks.length; j += 1) {
+      const grabProjectContainer =
+        document.querySelectorAll(".projectContainer");
+      const projectID = test[start].id;
+
+      for (
+        let project = 0;
+        project < grabProjectContainer.length;
+        project += 1
+      ) {
+        const containerID =
+          grabProjectContainer[project].getAttribute("data-projectid");
+        if (containerID === projectID) {
+          const middleRow =
+            grabProjectContainer[project].querySelector(".middleRow");
+          middleRow.appendChild(
+            createItemCard(
+              test[start].task[j].task,
+              test[start].task[j].notes,
+              test[start].task[j].priority,
+              test[start].task[j].date,
+              test[start].task[j].id
+            )
+          );
+        }
+      }
+
+      // console.log(test[start].task[j]);
+    }
+    // createItemCard(
+    //   test[start].tasks[start].task,
+    //   test[start].tasks[start].notes,
+    //   test[start].tasks[start].priority,
+    //   test[start].tasks[start].date
+    // );
   }
 };
 
