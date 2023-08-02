d73be8f47134556adb92037da184b1ce8571ba6e
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-08-02 17:15:55 -0400

    Allow user to create project and showcase on mainTab

--- src/components/Controller/addProject.js
+++ src/components/Controller/addProject.js
@@ -12,7 +12,8 @@ export default function addProject(e) {
   const item = createProject(titleInput.value, descriptionInput.value);
 
   createProjectTab(item.title, item.id);
-  createMainTab(item.title, item.description, item.id);
+  const createMain = createMainTab(item.title, item.description, item.id);
+  const createMainID = createMain.getAttribute("data-projectid");
 
   saveProjectSTodoLS("projects", projects);
 
@@ -20,4 +21,40 @@ export default function addProject(e) {
 
   titleInput.value = "";
   descriptionInput.value = "";
+
+  const grabAllProjectContainers =
+    document.querySelectorAll(".projectContainer");
+
+  for (let i = 0; i < grabAllProjectContainers.length; i += 1) {
+    const currentProjectID =
+      grabAllProjectContainers[i].getAttribute("data-projectid");
+
+    if (currentProjectID === createMainID) {
+      grabAllProjectContainers[i].classList.add("ONLINE");
+    } else {
+      grabAllProjectContainers[i].classList.add("OFFLINE");
+    }
+  }
+
+  // First, give our new created projectTab a class of ONLINE
+  // Next, we use a for loop to go through each project. IF class === true, console.log
+  // Else, set all other projects class to OFFLINE
+
+  // liElementContainer.addEventListener("click", (e) => {
+  //   const test = document.querySelector(".currentPage").children;
+
+  //   const currentID = e.currentTarget.id;
+
+  //   for (let i = 0; i < test.length; i += 1) {
+  //     if (currentID === test[i].getAttribute("data-projectid")) {
+  //       test[i].classList.add("ONLINE");
+  //       test[i].classList.remove("OFFLINE");
+  //     } else {
+  //       test[i].classList.add("OFFLINE");
+  //       test[i].classList.remove("ONLINE");
+  //     }
+  //   }
+
+  //   // Now, I need to make this specific ID from the mainPage be active or class active
+  // });
 }
